18236928f47e158b173544765531c77e85d6a567
SantiagoG1309 <kevxn_garcxa@outlook.com>
2025-04-30 12:04:22 -0500

    first commit

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -21,7 +21,7 @@ import os
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environment.get('SECRET_KEY', default='sdkjkdjaskjdslkajdk')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='sdkjkdjaskjdslkajdk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
